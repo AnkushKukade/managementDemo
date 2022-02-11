5f5c58495d2d3aaa37684abb0eac5e1f542ce7d9
AnkushKukade <ankushkukade1@gmail.com>
2022-02-11 15:41:50 +0530

    first.js file is edited

--- first.js
+++ first.js
@@ -1,3 +1,4 @@
 console.log("hello world!");
-console.warn("This is warning mesage");
-console.error("this is error message")
+console.log("****************************************************");
+console.log("****************************************************");
+console.log("****************************************************");
